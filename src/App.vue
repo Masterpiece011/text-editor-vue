02e019eaece9fc6edd71ea8801f3619bdfe60fed
Yura Korchagin <yura.korchagin.03@gmail.com>
2024-08-26 14:22:45 +0300

    complete task

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
     <v-main>
       <v-container>
         <div v-if="text.length > 0">
-          <text-editor :data="text"></text-editor>
+          <text-editor 
+          :data="text"
+          @change="changeText"
+          />
         </div>
         <div v-else>Идет загрузка...</div>
       </v-container>
@@ -33,13 +36,25 @@ export default {
         const response = await axios.get('https://66cbaa844290b1c4f19ad4a3.mockapi.io/api/text/texts').then(res => res = res.data);
         
         this.text = response
-        
-        
       } catch (error) {
         console.log('Error fetching data', error);
       }
-      console.log('text', this.text);
-      
+    },
+    async changeText(payload) {
+      console.log(payload);
+      try {
+        const response = await axios.put(`https://66cbaa844290b1c4f19ad4a3.mockapi.io/api/text/texts/${payload.id}`, {
+          text: payload.text, 
+          color: payload.color, 
+          font: payload.font,
+          alignment: payload.alignment,
+        });
+        
+        this.text = response
+      } catch (error) {
+        console.log('Error changing data', error);
+      }
+      this.fetchTexts()
     }
   },
   mounted() {
--- src/features/TextEditor/components/TextEditor.vue
+++ src/features/TextEditor/components/TextEditor.vue
@@ -1,50 +1,74 @@
 <template>
     <v-container>
-        <v-textarea v-model="text" label="Введите текст" />
+        <v-textarea
+            v-model="text"
+            label="Введите текст"
+            @keydown.enter.prevent="handleEnter" 
+        />
         <v-select v-model="font" :items="fontOptions" label="Выбор шрифта" />
         <v-select v-model="color" :items="colorOptions" label="Выбор цвета" />
         <v-select v-model="alignment" :items="alignmentOptions" label="Выбор выравнивания" />
-        <div :style="editorStyle">{{ text }}</div>
+        <v-btn @click="$emit('change', payload)">Сохранить</v-btn>
+
+        <v-container class="text-container">
+            <div :style="editorStyle" v-html="formattedText"></div>
+        </v-container>
     </v-container>
 </template>
 
 <script>
-import { ref, computed, watch } from 'vue';
+import { ref, computed } from 'vue'
 
 export default {
-props: {
-    data: {
-    type: Array,
-    required: true
+    props: {
+        data: {
+            type: Array,
+            required: true
+        }
+    },
+    setup(props) {
+        const text = ref(props.data[0].text)
+        const font = ref(props.data[0].font)
+        const color = ref(props.data[0].color)
+        const alignment = ref(props.data[0].alignment)
+
+        const fontOptions = ['Arial', 'Courier New', 'Georgia', 'Times New Roman']
+        const colorOptions = ['black', 'red', 'blue', 'green']
+        const alignmentOptions = ['left', 'center', 'right']
+
+        const editorStyle = computed(() => ({
+            fontFamily: font.value,
+            color: color.value,
+            textAlign: alignment.value
+        }))
+
+        const formattedText = computed(() => {
+            return text.value.replace(/\n/g, '<br>')
+        })
+
+        const handleEnter = (event) => {
+            const cursorPosition = event.target.selectionStart;
+            text.value = text.value.slice(0, cursorPosition) + '\n' + text.value.slice(cursorPosition)
+            event.target.setSelectionRange(cursorPosition + 1, cursorPosition + 1)
+        }
+
+        const payload = ref({
+            id: props.data[0].id,
+            text: text,
+            font: font,
+            color: color,
+            alignment: alignment
+        })
+
+        return { text, font, color, alignment, fontOptions, colorOptions, alignmentOptions, editorStyle, handleEnter, formattedText, payload }
     }
-},
-setup(props) {
-    const text = ref(props.data[0].text);
-    const font = ref(props.data[0].font);
-    const color = ref(props.data[0].color);
-    const alignment = ref(props.data[0].alignment);
-
-    const fontOptions = ['Arial', 'Courier New', 'Georgia', 'Times New Roman'];
-    const colorOptions = ['black', 'red', 'blue', 'green'];
-    const alignmentOptions = ['left', 'center', 'right'];
-
-    const editorStyle = computed(() => ({
-    fontFamily: font.value,
-    color: color.value,
-    textAlign: alignment.value
-    }));
-
-    // watch за изменением props.data
-    watch(() => props.data, (newVal) => {
-    if (newVal.length > 0) {
-        text.value = newVal[0].text;
-        font.value = newVal[0].font;
-        color.value = newVal[0].color;
-        alignment.value = newVal[0].alignment;
-    }
-    }, { immediate: true });
-
-    return { text, font, color, alignment, fontOptions, colorOptions, alignmentOptions, editorStyle };
 }
-};
 </script>
+
+<style>
+    .text-container {
+        margin-top: 40px;
+        background: #f0f0f0;
+        min-height: 56px;
+    }
+</style>
